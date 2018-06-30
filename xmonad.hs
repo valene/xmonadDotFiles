a09ed9617b1a49872480ca05d4db20a1238d2236
valene <branwen-mel-vlonylene@inbox.com>
2018-06-30 12:37:40 +0200

    added a bit of accumlated changes over the year

--- xmonad.hs
+++ xmonad.hs
@@ -4,6 +4,7 @@ import XMonad.Layout.NoBorders
 import XMonad.Layout.Spiral
 import XMonad.Layout.Tabbed
 import XMonad.Layout.ThreeColumns
+import XMonad.Layout.ResizableTile
 import XMonad.Hooks.FadeInactive
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
@@ -11,6 +12,7 @@ import XMonad.Hooks.ManageHelpers
 import XMonad.Util.Run(spawnPipe)
 import System.IO
 import System.Exit
+import Xmonad.Util.EZConfig
 
 main = do
   xmproc <- spawnPipe "xmobar $HOME/.xmobarrc"
@@ -28,7 +30,13 @@ main = do
         , ppCurrent = xmobarColor xmobarCurrentWorkspaceColor ""
         , ppSep = " "
         }
-    }
+    }  `additionalKeys`
+ 	[ ((0, xK_Print), spawn "scrot -d 1 -s ~/Pictures/screenshots/%Y-%m-%d-%T-screenshot.png"),
+	  ((mod4Mask, xK_Print), spawn "scrot -d 1 ~/Picture/screenshots/%Y-%m-%d-%T-screenshot.png"),
+          ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock"),
+	  ((mod4Mask .|. shiftMask, xK_a), sendMessage MirrorShrink),
+	  ((mod4Mask .|. shiftMask, xK_x), sendMessage MirrorExpand)
+	]
 
 --BorderWidth definition
 curBW = "3"
@@ -46,7 +54,7 @@ curLayout = avoidStruts (
   Mirror (Tall 1 (3/100) (1/2)))
 
 --Workspaces definition
-curWorkspaces = ["1:main","2:reading","3:monitoring","4:ssh","5","6","7","8","9"]
+curWorkspaces = ["1:main","2:reading","3:monitoring","4:ssh","5:qemu","6","7","8","9"]
 
 --terminal definition
 curTerminal = "urxvt"
